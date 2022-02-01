93158cfa5ac02e409fdf348c7244cfc39cdf6cc7
Nonpavit Detbun <non.devtech@gmail.com>
2022-02-01 17:46:56 +0700

    v1.0.0

---
+++ terraform/variables.tf
@@ -0,0 +1,15 @@
+variable "project_id" {
+  type = string
+  default = "createder-1535612021236"
+}
+
+variable "region" {
+  type = string
+  default = "asia-southeast1"
+}
+
+variable "bucket" {
+  type = string
+  default = "ntest-pipeline"
+}
+

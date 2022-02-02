385ced254e01a0eceba534ad82c8c87e8a0c2aef
Nonpavit Detbun <non.devtech@gmail.com>
2022-02-02 17:48:07 +0700

    edit config

---
+++ dataflow/build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+python pipeline.py \
+--input gs://${BUCKET}/data.csv \
+--output gs://${BUCKET}/output \
+--runner DataflowRunner \
+--project ${PROJECT} \
+--staging_location gs://${BUCKET}/staging \
+--temp_location gs://${BUCKET}/temp \
+--region ${REGION} \
+--save_main_session

d7c185456e08ea25300b884261e742408beadb91
Nonpavit Detbun <non.devtech@gmail.com>
2022-02-03 17:24:44 +0700

    a

--- dataflow/build.sh
+++ dataflow/build.sh
@@ -6,5 +6,5 @@ python pipeline.py \
 --staging_location gs://${BUCKET}/staging \
 --temp_location gs://${BUCKET}/temp \
 --region ${REGION} \
---template_location gs://${BUCKET}/templates/dataflow-demo-template
+--template_location gs://${BUCKET}/templates/dataflow-demo-template \
 --save_main_session
